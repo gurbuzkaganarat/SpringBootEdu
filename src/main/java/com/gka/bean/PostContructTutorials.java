0bbe4e5ce8571d334e41cd9b658f7cc92e203c63
Gürbüz Kağan Arat <163141400+gurbuzkaganarat@users.noreply.github.com>
2024-11-01 06:43:05 +0300

    thymeleaf model css js ekleme

--- src/main/java/com/gka/bean/PostContructTutorials.java
+++ src/main/java/com/gka/bean/PostContructTutorials.java
@@ -9,24 +9,24 @@ import java.util.logging.Logger;
 
 public class PostContructTutorials {
 
-    @Autowired
-    Logger LOG ;
-
-    // parametresiz constructor
-    // because this.og is null hatası alırsak post construct yazarak cozerız
-//    public PostContructTutorials() {
+//    @Autowired
+//    Logger LOG ;
+//
+//    // parametresiz constructor
+//    // because this.og is null hatası alırsak post construct yazarak cozerız
+////    public PostContructTutorials() {
+////        LOG.info("Log info Çağrıldı");
+////    }
+//
+//    //bean henüz baslamadıgından null pointer exception alıyoruz ve burada bağımlılıgı enjecte edemiyoruz .
+//    // Görevi : bir bean nesnesi olusturuldugunda hemen olusur ..
+//    @PostConstruct
+//    public void init(){
 //        LOG.info("Log info Çağrıldı");
 //    }
-
-    //bean henüz baslamadıgından null pointer exception alıyoruz ve burada bağımlılıgı enjecte edemiyoruz .
-    // Görevi : bir bean nesnesi olusturuldugunda hemen olusur ..
-    @PostConstruct
-    public void init(){
-        LOG.info("Log info Çağrıldı");
-    }
-    public static void main(String[] args) {
-        PostContructTutorials beanController = new PostContructTutorials();
-        System.out.println(beanController);
-
-    }
+//    public static void main(String[] args) {
+//        PostContructTutorials beanController = new PostContructTutorials();
+//        System.out.println(beanController);
+//
+//    }
 }
